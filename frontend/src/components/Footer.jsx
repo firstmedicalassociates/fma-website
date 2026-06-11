c1a1c289bc6dbf327c9b51a629b1c9bf8ba7b3ee
HoughCitryn <ghough@citryn.com>
2026-06-11 13:55:32 -0700

    Revert "INITIAL COMMIT"
    This reverts commit d53a8f67fdf89890644b0d73716726bd799dccc3.

--- frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Footer = () => {
-    return (
-        <footer className="footer flex flex-center width-100p">
-            <h5>Developed by Aavishkar Kolte, email: aavishkarkolte@gmail.com</h5>
-        </footer>
-    );
-};
